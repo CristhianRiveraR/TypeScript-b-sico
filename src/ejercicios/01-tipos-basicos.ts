2d7c65ab5863e5f8612d29bbe480154fd65f6c76
Cristhian Rivera <riverarodriguezcristhian@hotmail.com>
2021-05-18 09:29:18 -0500

    Se inicializa el repositorio

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,10 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Strider';
+let hp: number | string = 95;
+let estVivo: boolean = false;
+
+hp = 'FULL';
+console.log(nombre,hp);
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+interface Reproductor {
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalles;
+}
+
+interface Detalles {
+    autor: string;
+    anio: number;
+}
+
+const reproductor: Reproductor = {
+    volumen: 90,
+    segundo: 36,
+    cancion: 'Mes',
+    detalles:{
+        autor: 'Ed Sheeran',
+        anio: 2015
+    } 
+}
+
+const { volumen: vol, segundo, cancion, detalles} = reproductor;
+const { autor } = detalles;
+
+
+console.log('El volumen actual es de: ', vol);
+console.log('El segundo actual es de: ', segundo);
+console.log('La canción actual es: ', cancion);
+console.log('El autor es: ', autor);
