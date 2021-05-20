c3632800d9e3b791df721b810dc9489b90be7b42
Cristhian Rivera <riverarodriguezcristhian@hotmail.com>
2021-05-19 23:53:43 -0500

    Clases basicas

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
-import { calculaISV, Producto } from "./ejercicios/06-desestructuracion-funcion";
 
+class Heroe{
+    alterEgo: string;
+    edad: number;
+    nombreReal: number;
 
-const carritoCompras: Producto[] = [
-    {
-        desc: 'Telefono 1',
-        precio: 100
-    },
-    {
-        desc: 'Telefono 2',
-        precio: 150
-    },
-];
+    
+}
 
-const [total, isv] = calculaISV(carritoCompras);
+const ironman = new Heroe();
 
-console.log('Total:',total);
-console.log('ISV', isv);
+console.log(ironman);
 
 
